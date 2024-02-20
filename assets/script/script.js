c22f7a5b0f63384a07a241f7a308b19ba4bef26e
mehrabkb <mkordbacheh75@gmail.com>
2024-02-20 18:05:19 +0330

    add mobile header and mobile menu to project

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('#header .mobile-header .menu-btn button').on('click' , function(){
+        let that = $(this);
+        let icon = that.find('i');
+        $('.mobile-menu').toggleClass('show');
+        if($('.mobile-menu').hasClass('show')){
+            icon.removeClass('fa-bars');
+            icon.addClass('fa-arrow-right');
+        }else{
+            icon.removeClass('fa-arrow-right');
+            icon.addClass('fa-bars');
+        }
+    });
+})
